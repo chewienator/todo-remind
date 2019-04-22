6bb8704dd13837277737809ee642d32e4b7b069c
Carlos <6493@ait.nsw.edu.au>
2019-04-22 22:23:03 +1000

    missed one

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule' },
-  { path: 'task', loadChildren: './task/task.module#TaskPageModule' },
   { path: 'edit-task', loadChildren: './edit-task/edit-task.module#EditTaskPageModule' },
   { path: 'create-task', loadChildren: './create-task/create-task.module#CreateTaskPageModule' },
 ];
